f00ff936bb03cf23231fb674943d0b37b130d25e
NRakwax229 <150598223+NRakwax229@users.noreply.github.com>
2023-11-20 17:51:50 +0800

    Update PROJECT (calculate average score) aqqwa.cpp

--- PROJECT (calculate average score) aqqwa.cpp
+++ PROJECT (calculate average score) aqqwa.cpp
@@ -6,31 +6,28 @@ Programmer description:calculate the average score of 50 students
 date:15/11/2023
 */
 
-#include <iostream> // Include the iostream library
+#include <iostream> // Include the iostream library for input/output operations
 using namespace std; // Use the standard namespace
 
-int main() {    // Begin block
-    // Declare a constant integer for the number of scores
-    const int numScores = 50;
-    // Declare a variable to hold the sum of the scores
-    double sum = 0;
-    // Start a loop that runs for each score
-    for(int i = 0; i < numScores; i++)
+const int maximum = 50; // Define a constant for the maximum number of students
+int studentID[maximum]; // Array to store student IDs
+string studentNAME[maximum]; // Array to store student names
+double scores[maximum]; // Array to store student scores
+int overallStudent = 0; // Variable to keep track of the total number of students
+double averageofStudent = 0.0; // Variable to store the average score of students
+
+// Function to calculate the average score of students
+double calculateStudentAverageScores()
+{
+    double sum = 0.0; // Variable to store the sum of scores
+    for(int i = 0; i < overallStudent; i++) // Loop through each student
     {
-        // Declare a variable to hold the current score
-        double score;
-        // Prompt the user to enter their score
-        cout << "Enter your score" << (i+1) << ":";
-        // Read the score from the user
-        cin >> score;
-        // Add the score to the sum
-        sum += score;
+        sum += scores[i]; // Add the score of the current student to the sum
     }
-
-    // Calculate the average score
-    double average = sum / numScores;   
-    // display the average score
-    cout << "The average score is: " << average << endl;
-
-    return 0;
-}   // End block
+    if(overallStudent > 0) // Check if the number of students is greater than 0
+    {
+       averageofStudents = sum / overallStudent; // Calculate the average score
+    }
+    cout << "Average score of students: " << averageofStudents << endl; // Print the average score
+    return averageofStudents; // Return the average score
+}
